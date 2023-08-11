8a6b3b469ded5565b5e3150e94bc1e2722db31fb
thomashk2001 <thomashk2001@outlook.com>
2023-08-11 10:40:01 -0600

    Added template snippet for nav bar.

--- myshop/products/urls.py
+++ myshop/products/urls.py
@@ -3,8 +3,9 @@ from . import views # Both imports are needed for the url file.
 urlpatterns = [ # matched urls request views. 
     path('', views.index),
     # '' homepage , .index is a function in views, no () needed here
-    path('products', views.index),
-    path('products/<product>', views.product_cat), # dynamic url
-    path('signup', views.signup), # signup page, tried using products/signup didnt
+    path('products', views.index, name="home"), # third param name for using on href with django
+    path('products/<product>', views.product_cat, name="productcat"), # dynamic url
+    # to add more params use /<param01>/<param02>
+    path('signup', views.signup, name="signup"), # signup page, tried using products/signup didn't
     # work since it was getting captured by products/<product> so a different url was needed
 ]
