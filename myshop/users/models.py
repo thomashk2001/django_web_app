40eb55c40ca67a36bc9f254e21974a8ea9171562
thomashk2001 <thomashk2001@outlook.com>
2023-08-12 10:16:06 -0600

    Register user app, added a profile model

--- myshop/users/models.py
+++ myshop/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+  full_name: models.CharField(max_length= 100)
+  email: models.EmailField(max_length=254)
+  birthdate = models.DateField()
